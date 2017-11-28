6ee394a52ac519416454b0145f230ef88b3c20ee
kmolab <kmolab@mde.tw>
2017-11-28 16:40:04 +0800

    add comment to all methods

--- final_project/ui/Dialog.py
+++ final_project/ui/Dialog.py
@@ -23,54 +23,76 @@ class Dialog(QDialog, Ui_Dialog):
         """
         super(Dialog, self).__init__(parent)
         self.setupUi(self)
+        '''以下為使用者自行編寫程式碼區'''
 
     def digitClicked(self):
+        '''
+        使用者按下數字鍵, 必須能夠累積顯示該數字
+        當顯示幕已經為 0, 再按零不會顯示 00, 而仍顯示 0 或 0.0
+        
+        '''
         pass
         
     def unaryOperatorClicked(self):
+        '''單一運算元按下後處理方法'''
         pass
         
     def additiveOperatorClicked(self):
+        '''加或減按下後進行的處理方法'''
         pass
         
     def multiplicativeOperatorClicked(self):
+        '''乘或除按下後進行的處理方法'''
         pass
         
     def equalClicked(self):
+        '''等號按下後的處理方法'''
         pass
         
     def pointClicked(self):
+        '''小數點按下後的處理方法'''
         pass
         
     def changeSignClicked(self):
+        '''變號鍵按下後的處理方法'''
         pass
         
     def backspaceClicked(self):
+        '''回復鍵按下的處理方法'''
         pass
         
     def clear(self):
+        '''清除鍵按下後的處理方法'''
         pass
         
     def clearAll(self):
+        '''全部清除鍵按下後的處理方法'''
         pass
         
     def clearMemory(self):
+        '''清除記憶體鍵按下後的處理方法'''
         pass
         
     def readMemory(self):
+        '''讀取記憶體鍵按下後的處理方法'''
         pass
         
     def setMemory(self):
+        '''設定記憶體鍵按下後的處理方法'''
         pass
         
     def addToMemory(self):
+        '''放到記憶體鍵按下後的處理方法'''
         pass
         
     def createButton(self):
+        ''' 建立按鍵處理方法, 以 Qt Designer 建立對話框時, 不需要此方法'''
         pass
         
     def abortOperation(self):
+        '''中斷運算'''
         pass
         
     def calculate(self):
+        '''計算'''
         pass
